ccf41f715f0ab8971708b727d32767d3478d83f2
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-09-24 23:30:29 +0800

    Set data into form instance form redux action

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -11,18 +11,16 @@ import {
   FORM_SET_VALUE,
   FORM_VALIDATE,
   formEnhancer
-} from "./index";
-import {parseConfig} from "abstract-form/lib";
-import {Form} from "abstract-form/lib/form";
+} from './index';
+import {parseConfig} from 'abstract-form/lib';
+import {Form} from 'abstract-form/lib/form';
 
 describe('Abstract Form Enhancer', () => {
   describe('action FORM_INIT', () => {
     const spy = chai.spy();
 
     // @ts-ignore
-    const dummyReducer: Reducer<{}, AnyAction> = (state: any, action: AnyAction) => {
-      return state;
-    };
+    const dummyReducer: Reducer<{}, AnyAction> = (state: any, action: AnyAction) => state;
     const store = createStore(dummyReducer, {}, formEnhancer());
     store.subscribe(spy);
 
@@ -41,16 +39,13 @@ describe('Abstract Form Enhancer', () => {
           }
         ]
       }`;
-      const form = new Form(parseConfig(configString));
-
       store.dispatch(action(FORM_INIT, configString));
 
-      expect(spy).to.have.been.called();
-      expect(store.getState()).to.be.deep.equal({
-        $$abstractForm: {
-          form
-        }
-      });
+      expect(spy).to.have.been.called.once;
+      expect(store.getState()).to.have.deep.property('$$abstractForm').that.include(
+        {
+          data: {}
+        });
     });
   });
 
@@ -62,7 +57,42 @@ describe('Abstract Form Enhancer', () => {
 
   describe('action FORM_SET_VALUE', () => {
     it('should pass', () => {
+      const spy = chai.spy();
+      let configString = `{
+        "items": [
+          {
+            "id": "q1",
+            "dataType": "text",
+            "label": "Nick name"
+          },
+          {
+            "id": "q2",
+            "dataType": "number",
+            "label": "Year of birth"
+          }
+        ]
+      }`;
+      const dummyReducer: Reducer<{}, AnyAction> = (state: any, action: AnyAction) => state;
+      const store = createStore(dummyReducer, {}, formEnhancer());
+      store.subscribe(spy);
 
+      store.dispatch(action(FORM_INIT, configString));
+      store.dispatch(action(FORM_SET_VALUE, {
+        path: '$',
+        value: {
+          q1: 'Tom',
+          q2: 1987
+        }
+      }));
+
+      expect(spy).to.have.been.called.twice;
+      expect(store.getState()).to.have.deep.property('$$abstractForm').that.include(
+        {
+          data: {
+            q1: 'Tom',
+            q2: 1987
+          }
+        });
     });
   });
 
